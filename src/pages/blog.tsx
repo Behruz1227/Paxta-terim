4993f742e518b42ef58ff1b9840f938b03601b04
abdulvohid31 <abdulvohidcode@gmail.com>
2024-10-10 15:06:33 +0500

    sd

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,11 @@
-import { Typography } from '@mui/material';
+import { Table, TableContainer, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Scrollbar } from 'src/components/scrollbar';
 
 import { CONFIG } from 'src/config-global';
 import useGet from 'src/hooks/get';
+import { UserTableHead } from 'src/sections/user/user-table-head';
 
 export default function Page() {
   const {data, get, error, isLoading } = useGet()
@@ -18,7 +20,50 @@ export default function Page() {
       <Typography variant="h4" sx={{ mb: 5 }}>
         Machine
       </Typography>
-      
+      <Scrollbar>
+          <TableContainer sx={{ overflow: 'unset' }}>
+            <Table sx={{ minWidth: 800 }}>
+              <UserTableHead
+                order={table.order}
+                orderBy={table.orderBy}
+                rowCount={userData.length}
+                numSelected={table.selected.length}
+                onSort={table.onSort}
+                onSelectAllRows={(checked) =>
+                  table.onSelectAllRows(
+                    checked,
+                    userData.map((user) => user.id)
+                  )
+                }
+                headLabel={[
+                  { id: 'name', label: 'Ism' },
+                  { id: 'company', label: 'Familiya' },
+                  { id: 'role', label: 'Telefon raqam' },
+                  { id: 'isVerified', label: 'Lavozimi', align: 'center' },
+                  { id: 'status', label: 'Active' },
+                  { id: '' },
+                ]}
+              />
+              <TableBody>
+                {data && data.length === 0 ? (
+                  <TableNoData searchQuery={filterName} />
+                ) : (
+                  Array.isArray(data?.object) &&
+                  data?.object?.map((item: any) => (
+                      <UserTableRow
+                        key={item.id}
+                        row={item}
+                        selected={table.selected.includes(item.id)}
+                        onSelectRow={() => table.onSelectRow(item.id)}
+                        onDelete={() => deleteModal()}
+                        onEdit={() => editModal()}
+                      />
+                    ))
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Scrollbar>
     </div>
   );
 }
