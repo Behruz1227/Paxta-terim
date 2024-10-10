037dae5f48c97279ebccab3df65be7822983ff69
abdulvohid31 <abdulvohidcode@gmail.com>
2024-10-10 14:58:33 +0500

    hjk

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,17 +1,24 @@
 import { Typography } from '@mui/material';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import { CONFIG } from 'src/config-global';
+import useGet from 'src/hooks/get';
 
 export default function Page() {
+  const {data, get, error, isLoading } = useGet()
+  useEffect(() => {
+    get('/api/machine');
+  },[])
   return (
-    <>
+    <div className='container ml-10'>
       <Helmet>
         <title> {`Blog - ${CONFIG.appName}`}</title>
       </Helmet>
       <Typography variant="h4" sx={{ mb: 5 }}>
-        any more
+        Machine
       </Typography>
-    </>
+      
+    </div>
   );
 }
